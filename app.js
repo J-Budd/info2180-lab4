52daffcb3a29ced6ae5683e1ed7c0bd0c0441c2d
Jillian Budd <jillianabudd@gmail.com>
2024-11-17 14:56:45 -0500

    Search not working properly

--- app.js
+++ app.js
@@ -1,26 +1,22 @@
 "use strict";
 const httpRequest = new XMLHttpRequest();
 
+
 document.addEventListener('DOMContentLoaded', () => {
-    let search = document.querySelector("button");
-    search.addEventListener("click", showHeroes);
+    const search = document.querySelector("button");
+    search.addEventListener("click", function() {
+        const url = `http://localhost/info2180-lab4/superheroes.php`;
+
+        httpRequest.onreadystatechange = function() {
+            if (httpRequest.readyState === XMLHttpRequest.DONE) {
+                if (httpRequest.status === 200) {
+                    const response = httpRequest.responseText;
+                } else {
+                    alert('There was a problem with the request.');
+                }
+            }
+        };
+        httpRequest.open('GET', url);
+        httpRequest.send();
+    });
 });
-
-
-function showHeroes(){
-    let url = "http://localhost/info2180-lab4/superheroes.php";
-    httpRequest.onreadystatechange = handler;
-    httpRequest.open('GET', url);
-    httpRequest.send();
-}
-
-function handler(){
-    if (httpRequest.readyState === XMLHttpRequest.DONE) {
-        if (httpRequest.status === 200) {
-            let response = httpRequest.responseText;
-            alert(response);
-        } else {
-            alert('There was a problem with the request.');
-        }
-    }
-}
